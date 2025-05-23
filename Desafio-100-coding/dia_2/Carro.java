fd463ed53010d381387342f2c8184ee4f8370f6d
Paulo Henrique <127267437+AshSlake@users.noreply.github.com>
2025-05-23 00:20:52 -0300

    feat(add): 🎉 dia 2 contrutores de classe.
    aprendizado sobre os contrutores e como eles deixam o codigo mais
    modular e com menos linhas de codigo ao instanciar a classe passando os
    paramentros direto na instancia.

---
+++ Desafio-100-coding/dia_2/Carro.java
@@ -0,0 +1,44 @@
+package dia_2;
+
+/*
+ * Dia 2 do #100daysofcode, hoje vimos sobre construtores e a forma de definilos tornando o codigo mais limpo, organizado e modular.
+ * */
+
+public class Carro {
+	String marca;
+	String modelo;
+	int numPassageiros;
+	double capCombustivel;
+	double consumoCombustivel;
+	
+	//toda classe ja vem com um contrutor vazio por padrão, 
+	//mais podemos declara-lá para definir parametros iniciais padrões.
+	Carro() {
+		System.out.println("A classe Carro foi instanciada.");
+	}
+	
+	// podemos criar um construtor com quantos parametros quisermos, assim como todos os da classe.
+	Carro(String marca_, String modelo_, int numPassageiros_, double capCombustivel_, double consumoCombustivel_){
+		marca = marca_;
+		modelo = modelo_;
+		numPassageiros = numPassageiros_;
+		capCombustivel = capCombustivel_;
+		consumoCombustivel = consumoCombustivel_;
+	}
+	
+	void exibirAutonomia() {
+		
+	    System.out.println("A autonomia do carro é: " + capCombustivel * consumoCombustivel + " km");
+	}
+	
+	double obterAutonomia() {
+		
+		System.out.println("Método obterAutonomia foi chamado.");
+		return capCombustivel * consumoCombustivel;
+	}
+	
+	double calcularCombustivel(double km) {
+		return km/consumoCombustivel;
+	}
+
+}
---
+++ Desafio-100-coding/dia_2/testeCarro.java
@@ -0,0 +1,24 @@
+package dia_2;
+
+public class testeCarro {
+
+	public static void main(String[] args) {
+		Carro van = new Carro();
+		van.marca = "Fiat";
+		van.modelo = "Van";
+		van.numPassageiros = 10;
+		van.capCombustivel = 100;
+		van.consumoCombustivel = 0.2;
+		
+		Carro carro = new Carro("volkswagen", "gol", 4, 51, 9);
+		
+		System.out.println(carro.marca);
+		System.out.println(carro.modelo);
+		System.out.println(carro.numPassageiros);
+		System.out.println(carro.capCombustivel);
+		System.out.println(carro.consumoCombustivel);
+	}
+	
+	
+
+}
