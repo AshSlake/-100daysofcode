1c22b675dbd9fdcffeb0cb9956b2deda5e2aa824
Paulo Henrique <127267437+AshSlake@users.noreply.github.com>
2025-05-23 23:24:43 -0300

    feat(add): 🎉 dia 3 aprendendo sobre THIS. e modificadores de acesso(public e private)

---
+++ Desafio-100-coding/src/dia_3/Carro.java
@@ -0,0 +1,49 @@
+package dia_3;
+
+/*
+ * Dia 3 do #100daysofcode
+ *  - falando sobre o parametro "THIS".
+ *  - vimos tambem sobre modificadores de acesso public e private.
+ * */
+
+public class Carro {
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+
+
+    Carro(String marca, String modelo, int numPassageiros, double capCombustivel, double consumoCombustivel){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.numPassageiros = numPassageiros;
+        this.capCombustivel = capCombustivel;
+        this.consumoCombustivel = consumoCombustivel;
+    }
+
+    // chamando o construtor existente dentro de um novo construtor:
+    public Carro(String marca, String modelo) {
+        this(marca, modelo, 0, 0, 0);
+    }
+
+    void exibirAutonomia() {
+
+        System.out.println("A autonomia do carro é: " + this.capCombustivel * this.consumoCombustivel + " km");
+    }
+
+    double obterAutonomia() {
+
+        System.out.println("Método obterAutonomia foi chamado.");
+        return this.capCombustivel * this.consumoCombustivel;
+    }
+
+    double calcularCombustivel(double km) {
+        return km/this.consumoCombustivel;
+    }
+
+
+}
+
+
+
---
+++ Desafio-100-coding/src/dia_3/testeCarro.java
@@ -0,0 +1,10 @@
+package dia_3;
+
+public class testeCarro {
+    public static void main(String[] args) {
+
+        Carro newCarro = new Carro("Volkswagen","Gol");
+
+        System.out.println(newCarro.marca);
+    }
+}
